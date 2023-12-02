601a26182de5231718ebd2632ca33ca7ebccd686
Jakob Rönström <jakob@ronstrom.net>
2023-12-03 00:25:12 +0100

    Solution day 1

--- day01/index.js
+++ day01/index.js
@@ -8,23 +8,63 @@ function inputDataLinesStrings(filename="input.txt") {
     return fs.readFileSync(filename).toString().trim().split("\n")
 }
 
-function getSolutionPart1() {
+function getSolutionPart1(lines) {
     console.log("Runing part 1")
-    return inputDataLinesIntegers().reduce((x,y)=>x+y)
+    return lines.map((str) => str.match(/\d/g)).map((x) => parseInt(x[0]+x[x.length-1])).reduce((x,y)=>x+y)
 }
 
-function getSolutionPart2() {
-    console.log("Runing part 2")
-    return inputDataLinesIntegers().reduce((x,y)=>x*y)
+function getSolutionPart2(lines) {
+    let first = lines.map((str) => firstDigit(str))
+    let last = lines.map((str) => lastDigit(str))
+    
+    var sum = 0
+    for (var i=0;i<lines.length;i++) {
+        let num = parseInt(first[i]+last[i])
+        sum += num
+    }
+
+    return sum
+}
+
+function firstDigit(str) {
+    var map = { 1: "1", 2: "2", 3: "3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", one: "1", two: "2", three: "3", four: "4", five: "5", six: "6", seven: "7", eight: "8", nine: "9"}
+
+    let indexs = Object.keys(map).map((key) => [key, str.indexOf(key)])
+    var min = Number.MAX_SAFE_INTEGER
+    var key = map[1]
+    for (var i=0;i<indexs.length;i++) {
+        if (indexs[i][1] != -1 && indexs[i][1]<min) {
+            min = indexs[i][1]
+            key = indexs[i][0]
+        }
+    }
+
+    return map[key]
+}
+
+function lastDigit(str) {
+    var map = { 1: "1", 2: "2", 3: "3", 4:"4", 5:"5", 6:"6", 7:"7", 8:"8", 9:"9", one: "1", two: "2", three: "3", four: "4", five: "5", six: "6", seven: "7", eight: "8", nine: "9"}
+
+    let indexs = Object.keys(map).map((key) => [key, str.lastIndexOf(key)])
+    var max = -1
+    var key = map[1]
+    for (var i=0;i<indexs.length;i++) {
+        if (indexs[i][1] != -1 && indexs[i][1]>max) {
+            max = indexs[i][1]
+            key = indexs[i][0]
+        }
+    }
+
+    return map[key]
 }
 
 const part = process.env.part || "part1"
 
 if (part === "part1")
-    console.log(getSolutionPart1())
+    console.log(getSolutionPart1(inputDataLinesStrings()))
 else
-    console.log(getSolutionPart2())
+    console.log(getSolutionPart2(inputDataLinesStrings()))
 
 module.exports = {
-    getSolutionPart1, getSolutionPart2, inputDataLinesIntegers
+    getSolutionPart1, getSolutionPart2
 }
--- day01/test/index.test.js
+++ day01/test/index.test.js
@@ -1,9 +1,12 @@
 const {getSolutionPart1, getSolutionPart2} = require('../index')
 
+let testData = ['1abc2', 'pqr3stu8vwx', 'a1b2c3d4e5f', 'treb7uchet']
+let testData2 = ['two1nine', 'eightwothree', 'abcone2threexyz', 'xtwone3four', '4nineeightseven2', 'zoneight234', '7pqrstsixteen']
+
 test('part1', ()=>{
-    expect(getSolutionPart1()).toBe(1379)
+    expect(getSolutionPart1(testData)).toBe(142)
 })
 
 test('part2', ()=>{
-    expect(getSolutionPart2()).toBe(56154)
+    expect(getSolutionPart2(testData2)).toBe(281)
 })
